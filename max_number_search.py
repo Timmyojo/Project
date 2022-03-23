f463948e599ad1fc4b782c5038d36d9525716c50
Timmyojo <Timmygroup1@gmail.com>
2022-03-23 17:49:58 +0100

    first version of maximunm number search code

---
+++ max_number_search.py
@@ -0,0 +1,13 @@
+import numbers
+
+
+def max_number(numbers):
+    max_number = numbers[0]
+    for number in numbers:
+        if number > max_number:
+            max_number = number
+    return max_number
+
+num = max_number([4, 90])
+print(num)
+
